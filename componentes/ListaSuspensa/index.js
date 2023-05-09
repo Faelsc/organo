ddd9d514defe472a053843c12978d2150eecff2d
Rafael Santos Corrêa <101223032+Faelsc@users.noreply.github.com>
2023-05-09 14:50:20 -0300

    :construction:

---
+++ componentes/ListaSuspensa/index.js
@@ -0,0 +1,17 @@
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+    console.log(props.itens)
+    return (
+        <div className='lista-supensa'>
+            <label>{props.label}</label>
+            <select required={props.obrigatorio}>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
